6e91bdcf874dcf19b4cf2f7255acdd25632254ae
anujYadav <anujvns28@gmail.com>
2024-07-04 17:16:20 +0530

    payment system done

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const authRoutes = require("./routes/atuh")
 const categoriesRoutes = require("./routes/categories");
 const subCategoriesRoutes = require("./routes/subCategories");
 const productRoutes = require("./routes/product");
+const paymentRoutes = require("./routes/payment");
 const { cloudinaryConnect } = require('./config/cloudenary');
 
 
@@ -41,6 +42,7 @@ app.use("/api/v1/auth",authRoutes)
 app.use("/api/v1/category",categoriesRoutes)
 app.use("/api/v1/subCategory",subCategoriesRoutes)
 app.use("/api/v1/product",productRoutes)
+app.use("/api/v1/payment",paymentRoutes)
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
